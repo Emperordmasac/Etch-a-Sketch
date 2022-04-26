2c743eb1a352a49b32b3cee1e886db7a0020cc29
Olamilekan <abiolawalter2015@gmail.com>
2022-04-26 10:53:27 +0100

    wrote the function to setup the grid system

--- app.js
+++ app.js
@@ -54,3 +54,20 @@ function clearGrid() {
 
 //MAIN FUNCTIONS
 //function to setup the grid system
+function setupGridSystem(size) {
+    //
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add("grid-element")
+        Grid.appendChild(gridElement)
+    }
+
+}
+
+//onMount 
+window.onload = () => {
+    setupGridSystem(defaultSize);
+}
